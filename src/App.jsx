85a2b53328fbf7022da4f9500a6e4ac00ef3bda6
Minte <mintesinottamene0917@gmail.com>
2025-01-04 10:55:13 +0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
