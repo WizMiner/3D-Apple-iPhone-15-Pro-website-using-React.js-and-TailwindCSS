88f2d9def5fc6bece7980ec8195fda4add18d490
Minte <mintesinottamene0917@gmail.com>
2025-01-04 10:06:08 +0300

    config to dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Ensures the app is accessible externally
+    port: 3000, // You can choose any available port
+  },
+  build: {
+    // Explicitly define the output directory
+    outDir: 'dist',
+
+    // Enable manual chunks to split large libraries into separate files
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into a separate chunk to reduce main file size
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+
+    // Increase chunk size limit if needed
+    chunkSizeWarningLimit: 1500, // default is 500 KB, increase to avoid warnings
+
+    // Optimize asset handling
+    assetsInlineLimit: 4096, // Inline assets smaller than 4KB into the build, adjust as needed
+  }
+});
